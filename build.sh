4f053858417160aaba9333dbaa9b40cd5b546af5
FliegendeWurst <2012gdwu+github@posteo.de>
2021-04-14 17:05:07 +0200

    build script

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+rustup target add x86_64-unknown-linux-musl
+cargo build --release --target x86_64-unknown-linux-musl
+strip target/release/KIT-ILIAS-downloader
+
+rustup target add x86_64-pc-windows-gnu
+# if on Debian or similar
+sudo apt install mingw-w64
+cargo build --release --target x86_64-pc-windows-gnu
+strip target/x86_64-pc-windows-gnu/release/KIT-ILIAS-downloader
