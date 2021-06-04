1ff1f1417a0362f7a556fba84f61ec0080cf6c68
FliegendeWurst <2012gdwu+github@posteo.de>
2021-06-04 10:13:33 +0200

    build script: require up to date lock file

--- build.sh
+++ build.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 rustup target add x86_64-unknown-linux-musl
-cargo build --release --target x86_64-unknown-linux-musl
-strip target/release/KIT-ILIAS-downloader
+cargo build --locked --release --target x86_64-unknown-linux-musl
 
 rustup target add x86_64-pc-windows-gnu
 # if on Debian or similar
 sudo apt install mingw-w64
-cargo build --release --all-features --target x86_64-pc-windows-gnu
-strip target/x86_64-pc-windows-gnu/release/KIT-ILIAS-downloader.exe
+cargo build --locked --release --all-features --target x86_64-pc-windows-gnu
